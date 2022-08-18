b6c898760f82a84feebd98b10f5feffb28f4ea71
Md Mamun <juborajvai22@gmail.com>
2022-08-18 12:14:57 +0600

    order route start

--- controler/orderControler.js
+++ controler/orderControler.js
@@ -31,11 +31,45 @@ exports.newOrder = async (req , res , next) =>{
 
 exports.getAllOrders = async (req , res , next) =>{
   const orders = await Order.find()
-  console.log(orders);
-
   res.status(200).json({
     success: true,
     orders
 
   })
+}
+
+exports.getSingleOrder = async (req , res , next) =>{
+  const id = req.params.id
+  console.log(typeof id);
+  const order = await Order.findById(id).populate("user" ,  "name email")
+  if(!order){
+    res.status(404).json({
+      success: false,
+      "message": "Order Not found!"
+    });
+
+  }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+}
+
+exports.orderDelete = async (req , res , next) =>{
+  const order = await Order.findById(req.params.id)
+  if(!order){
+    res.status(404).json({
+      success: false,
+      "message": "Order Not found!"
+    });}
+
+    order.remove()
+    res.status(200).json({
+      success: true,
+      "message": "Order Delete Successfull"
+    })
+}
+
+exports.orderUpdate = async (req , res, next) =>{
+  
 }
--- router/orderRoute.js
+++ router/orderRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { newOrder, getAllOrders } = require('../controler/orderControler');
+const { newOrder, getAllOrders, getSingleOrder, orderDelete } = require('../controler/orderControler');
 const router = express.Router()
 
 
 router.post("/new/:id" , newOrder)
 router.get("/" , getAllOrders)
+router.get("/:id" , getSingleOrder)
+router.delete("/:id" , orderDelete)
 
 
 
