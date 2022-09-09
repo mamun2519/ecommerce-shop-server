04ccb049b68caebffce7d1a0bd423851008533e7
Md Mamun <juborajvai22@gmail.com>
2022-09-09 19:29:23 +0600

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fileUpload = require("express-fileupload");
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-      origin: "https://fashion-ecommerce-92924.web.app"
+      origin: "http://localhost:3000"
 }))
 app.use(fileUpload());
 app.use(express.static("public"));
