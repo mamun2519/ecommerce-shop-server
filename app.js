1dd1a479235c7dd02c3a86d3af1f8cbdad388add
Md Mamun <juborajvai22@gmail.com>
2022-09-12 20:48:47 +0600

    update

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 app.use(cors({
-      origin: "https://fashion-ecommerce-92924.web.app",
-      // methods: ["get" , "post"],
+      origin: "http://localhost:3000",
+      // methods: ["get" , "pos"],
     
      
       
