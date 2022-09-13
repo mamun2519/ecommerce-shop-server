34ed9f2a7c0f29aff1d3e99db92875aaa6229639
Md Mamun <juborajvai22@gmail.com>
2022-09-13 23:19:57 +0600

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 app.use(cors({
       origin: "http://localhost:3000",
-      // methods: ["get" , "s"],
+      // methods: ["get" ,"],
     
      
       
