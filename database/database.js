7d869990c92fade6f8c7600e1874c1267a8248b0
Md Mamun <juborajvai22@gmail.com>
2023-01-07 14:00:45 +0600

    update

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const database = () =>{
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@mamun.rd1yf.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://mamun:Ej3H6A21xJJwHkBi@mamun.rd1yf.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(uri , { useNewUrlParser: true, useUnifiedTopology: true })
 .then((data) => {
